c59d84ad6cfeec735552e8bfa9430e91a34b1683
kamilanov18 <kamilanov18@codingburgas.bg>
2020-01-21 23:12:05 +0200

    Add data.js and make GetDataScript.js's getInfo function use arguments

--- js/GetDataScript.js
+++ js/GetDataScript.js
@@ -1,43 +1,18 @@
-
-function getCode(searchCode,url)
+// the function takes two parameters. First: the DOM path of the desired element in the targeted website. Second: The selctor for the place where the data will be placed. Third: The targeted website's URL.
+function getInfo(searchElement,placeElement,url)
 {
-    $("#population_value").text("Retrieving data...");
-
-    var codeString;
     var content; // this variable will store all of the targeted website's html code
     var cross = "http://www.whateverorigin.org/get?url="; // This variable stores the URL of the proxy server, used to bypass the CORS policy
     $.getJSON(cross + encodeURIComponent(url) + '&callback=?', function(data)// this is getJSON request. We search for the targeted website's adress trough the proxy server
     {
         content=data.contents;// Fill the content variable with all of the targeted website's html code
+        var parsedHtml = $.parseHTML(content); // Parse all of the targeted website's code in a variable called parsedHtml
+        var dvPopulation = $(parsedHtml).find(searchElement).text();// Give the dvPopulation variable the value of the desired element.
+        $(placeElement).html(dvPopulation);// Replace the element known as placeElement's html with the value of dvPopulation
 
-        // console.log(content); // Print all of the targeted webside's code
-        // console.log(typeof content); // Print the variable type of content
-
-        // var last_index = content.lastIndexOf(searchCode); //Search for the specific word "maincounter-number"'s last index and we store it in a variable     
-        // console.log(last_index); // Print the value that holds the last index of the keyword
-
-        // console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        let parsedHtml = $.parseHTML(content);
-        console.log(parsedHtml);
-
-        let dvPopulation = $(parsedHtml).find("div.country-pop-description li:first-child strong:last-child").text();
-        console.log(dvPopulation);
-
-        let sp1 = $("<span>")
-            .text("The population of Gernany is: " + dvPopulation + " people. Incredible!");
-
-            $("#population_value").html(sp1);
-
-        // setTimeout(function(){
-        //     getCode(searchCode,url);
-        // }, 3000);
-
-        // for(var i=0;i<100;i++)// Make a for loop that concatinates every next element into 1 usable string
-        // {
-        //     codeString+=content[last_index+i]; //k concatinates every next i, to form the piece of code where the desired number would be
-        // }
-        // console.log(codeString);// Print the desired string
+        // This snippet of code makes sure to get the desired (live) data over a 3 second period of time
+        setTimeout(function(){
+            getInfo(searchElement,placeElement,url);
+        }, 3000);
     });
-
-    
 }
---
+++ js/data.js
@@ -0,0 +1,12 @@
+var dataArray = [ // this array contains the URLs of all the websites we get our data from
+    {
+        country:"japan",
+        url:"http://www.oceanhealthindex.org/region-scores/scores/japan",
+        dataPoint:"span.score.score-lrg"
+    },
+    {
+        country:"russia",
+        url:"http://www.oceanhealthindex.org/region-scores/scores/russia",
+        dataPoint:"span.score.score-lrg"
+    }
+];
