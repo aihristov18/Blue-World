c45975201a0689e2e7c8fb7bd2522688c28c090d
Александър Игоров Христов <aihristov18@codingburgas.bg>
2020-01-23 14:06:12 +0200

    add more information to data.js (part 1)

--- js/data.js
+++ js/data.js
@@ -8,5 +8,10 @@ var dataArray = [ // this array contains the URLs of all the websites we get our
         country:"russia",
         url:"http://www.oceanhealthindex.org/region-scores/scores/russia",
         dataPoint:"span.score.score-lrg"
-    }
+    },
+    {
+        country:"china",
+        url:"http://www.oceanhealthindex.org/region-scores/scores/china",
+        dataPoint:"span.score.score-lrg"
+    },
 ];
