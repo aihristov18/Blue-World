bd6a3ecdeb649ff05cd3a4b81a921e78e517d7e8
kamilanov18 <kamilanov18@codingburgas.bg>
2020-01-23 17:22:38 +0200

    Add Initialise() function to generate svg background

--- js/GetDataScript.js
+++ js/GetDataScript.js
@@ -16,3 +16,20 @@ function getInfo(searchElement,placeElement,url)
         }, 3000);
     });
 }
+
+// Takes 3 arguments. Width: how many triangles will be generated along the X axis. Height: how many triangles alod the Y axis. placeelement: where to place the generated backgroud
+function Initialise(width,height,placeElement) 
+{
+    var codeString='<div class="container" style="width:100%; height:100%;"><svg id="triangle" viewBox="0 0 3700 3700">'; // this string stores all of the code for the bakground
+    for(var y=0;y<height;y++) // Iterate trough every
+    {
+        for(var x=0;x<width;x++)
+        {
+            codeString+="<polygon id='x"+x+"y"+y+"' points='"+(x*100+50)+" "+y*100+", "+(x*100+100)+" "+((y*100)+(100))+", "+(x*100)+" "+((y*100)+(100))+"' />";
+            codeString+="<polygon id='x!"+x+"y!"+y+"' points='"+(x*100+100)+" "+((y*100)+(100))+", "+((x*100)+150)+" "+(y*100)+", "+(x*100+50)+" "+y*100+"'/>"
+        }
+    }
+    codeString+="</svg></div>";
+    console.log(codeString);
+    document.getElementById(placeElement).innerHTML=codeString;
+}
