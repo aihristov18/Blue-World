7c61412672debc1ad3e662a467dc64b274732d0f
kamilanov18 <kamilanov18@codingburgas.bg>
2020-01-29 09:47:11 +0200

    Fix ID problematic IDs in html.html and GetDataScript.js

--- js/GetDataScript.js
+++ js/GetDataScript.js
@@ -18,19 +18,22 @@ function getInfo(searchElement, placeElement, url) {
 
 // Takes 3 arguments. Width: how many triangles will be generated along the X axis. Height: how many triangles alod the Y axis. placeelement: where to place the generated backgroud
 function InitialiseMap(width, height, placeElement) {
+    var XIDcounter=0;
     var codeString = '<div class="container" style="width:100%; height:100%;"><svg id="triangle" viewBox="0 0 3700 3700">'; // this string stores all of the code for the bakground
     for (var y = 0; y < height; y++) // Iterate trough every
     {
         for (var x = 0; x < width; x++) {
             if (y % 2 == 0) {
-                codeString += "<polygon id='x" + x + "y" + y + "' points='" + (x * 100) + " " + (y * 85) + ", " + ((x * 100) + 50) + " " + ((y * 85) + 85) + ", " + ((x * 100) - 50) + " " + ((y * 85) + 85) + "' />";
-                codeString += "<polygon id='x!" + x + "y!" + y + "' points='" + ((x * 100) + 50) + " " + ((y * 85) + 85) + ", " + ((x * 100) + 100) + " " + ((y * 85)) + ", " + (x * 100) + " " + (y * 85) + "' />"
+                codeString += "<polygon id='x" + XIDcounter*2+1 + "y" + y + "' points='" + (x * 100) + " " + (y * 85) + ", " + ((x * 100) + 50) + " " + ((y * 85) + 85) + ", " + ((x * 100) - 50) + " " + ((y * 85) + 85) + "' />";
+                codeString += "<polygon id='x" + XIDcounter*2+2 + "y" + y + "' points='" + ((x * 100) + 50) + " " + ((y * 85) + 85) + ", " + ((x * 100) + 100) + " " + ((y * 85)) + ", " + (x * 100) + " " + (y * 85) + "' />"
             }
             else {
-                codeString += "<polygon id='x" + x + "y" + y + "' points='" + (x * 100) + " " + ((y / 2 + 0.5) * 170) + ", " + ((x * 100) + 50) + " " + ((y * 85)) + ", " + ((x * 100) - 50) + " " + ((y * 85)) + "' />";
-                codeString += "<polygon id='x!" + x + "y!" + y + "' points='" + ((x * 100) + 50) + " " + ((y * 85)) + ", " + ((x * 100) + 100) + " " + ((y / 2 + 0.5) * 170) + ", " + (x * 100) + " " + ((y / 2 + 0.5) * 170) + "' />";
+                codeString += "<polygon id='x" + XIDcounter*2+1 + "y" + y + "' points='" + (x * 100) + " " + ((y / 2 + 0.5) * 170) + ", " + ((x * 100) + 50) + " " + ((y * 85)) + ", " + ((x * 100) - 50) + " " + ((y * 85)) + "' />";
+                codeString += "<polygon id='x" + XIDcounter*2+2 + "y" + y + "' points='" + ((x * 100) + 50) + " " + ((y * 85)) + ", " + ((x * 100) + 100) + " " + ((y / 2 + 0.5) * 170) + ", " + (x * 100) + " " + ((y / 2 + 0.5) * 170) + "' />";
             }
+            XIDcounter++;
         }
+        XIDcounter=0;
     }
     codeString += "</svg></div>";
     console.log(codeString);
@@ -49,3 +52,13 @@ function ded(width, height) {
         $(string2).toggleClass("water");
     }, 3000);
 }
+//0
+//1 2
+//1     1*1+1
+//3 4
+//2     2*2+1
+//5 6
+//3     3*2+1
+//7 8
+//4
+//9 10
