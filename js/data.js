a4b205ac113334e4814dca335cbfb296c2f57b48
kamilanov18 <kamilanov18@codingburgas.bg>
2020-03-01 21:01:50 +0200

    Bug Fixes * Rename Map titles to fit the numbers * Rename the countries' names

--- js/data.js
+++ js/data.js
@@ -1,84 +1,84 @@
 var dataArray = [ // this array contains the URLs of all the websites we get our data from
     {
         country: "#Japan",                                                                 // 'country' hold the ID, by which this object is selected and used
-        name: "Japan",                                                                     // This is the name of the country, that will be displayed on the databoard
+        name: "Япония",                                                                     // This is the name of the country, that will be displayed on the databoard
         populationUrl: "https://www.worldometers.info/world-population/japan-population/", //'populationUrl' holds the url of the site where we get our population number; It is updated daily;
         GDPUrl: "https://www.worldometers.info/gdp/japan-gdp/",                            //'GDPUrl' holds the url of the site where we get our GDP number. For now it doesn't update
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/japan",               //'FPUrl' holds the url of the site where we get our pollution and food production score index. We are unsure of when they update
     },
     {
         country: "#Russia",
-        name: "Russian Federation",
+        name: "Руска Федерация",
         populationUrl: "https://www.worldometers.info/world-population/russia-population/",
         GDPUrl: "https://www.worldometers.info/gdp/russia-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/russia",
     },
     {
         country: "#China",
-        name: "People's Republic of China",
+        name: "Китай",
         populationUrl: "https://www.worldometers.info/world-population/china-population/",
         GDPUrl: "https://www.worldometers.info/gdp/china-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/china",
     },
     {
         country: "#USA",
-        name: "United States of America",
+        name: "Съединените Американски Щати",
         populationUrl: "https://www.worldometers.info/world-population/us-population/",
         GDPUrl: "https://www.worldometers.info/gdp/us-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/united-states",
     },
     {
         country: "#Canada",
-        name: "Canada",
+        name: "Канада",
         populationUrl: "https://www.worldometers.info/world-population/canada-population/",
         GDPUrl: "https://www.worldometers.info/gdp/canada-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/canada",
     },
     {
         country: "#India",
-        name: "India",
+        name: "Индия",
         populationUrl: "https://www.worldometers.info/world-population/india-population/",
         GDPUrl: "https://www.worldometers.info/gdp/india-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/india",
     },
     {
         country: "#Germany",
-        name: "Germany",
+        name: "Германия",
         populationUrl: "https://www.worldometers.info/world-population/germany-population/",
         GDPUrl: "https://www.worldometers.info/gdp/germany-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/germany",
     },
     {
         country: "#France",
-        name: "France",
+        name: "Франция",
         populationUrl: "https://www.worldometers.info/world-population/france-population/",
         GDPUrl: "https://www.worldometers.info/gdp/france-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/france",
     },
     {
         country: "#UK",
-        name: "The United Kingdom of Great Britain and Northern Ireland",
+        name: "Съединеното кралство",
         populationUrl: "https://www.worldometers.info/world-population/uk-population/",
         GDPUrl: "https://www.worldometers.info/gdp/uk-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/united-kingdom",
     },
     {
         country: "#Bulgaria",
-        name: "Bulgaria",
+        name: "България",
         populationUrl: "https://www.worldometers.info/world-population/bulgaria-population/",
         GDPUrl: "https://www.worldometers.info/gdp/bulgaria-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/bulgaria",
     },
     {
         country: "#Mexico",
-        name: "Mexico",
+        name: "Мексико",
         populationUrl: "https://www.worldometers.info/world-population/mexico-population/",
         GDPUrl: "https://www.worldometers.info/gdp/mexico-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/mexico",
     },
     {
         country: "#Australia",
-        name: "Australia",
+        name: "Австралия",
         populationUrl: "https://www.worldometers.info/world-population/australia-population/",
         GDPUrl: "https://www.worldometers.info/gdp/australia-gdp/",
         FPUrl: "http://www.oceanhealthindex.org/region-scores/scores/australia",
